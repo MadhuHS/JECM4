c5d9c647f7fb585daf450fe306a3b36550183f90
MadhuHS <madhuhs943@gmail.com>
2016-01-28 13:33:03 +0530

    Codes till Login page
    28/01/2016

--- Login/src/com/jsp/login/MainActivity.java
+++ Login/src/com/jsp/login/MainActivity.java
@@ -1,22 +1,57 @@
 package com.jsp.login;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	
+	TextView logotextview,signuptextview,forgotpassswordtextview;
+	EditText usernameedittext,passwordedittedt;
+	CheckBox rembrpwdcheckbox;
+	Button loginbutton;
+	
+	String stUsername = "abc";
+	String stPassword = "1234";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		usernameedittext = (EditText) findViewById(R.id.editTextusername);
+		passwordedittedt = (EditText) findViewById(R.id.editTextpassword);
+		loginbutton = (Button) findViewById(R.id.buttonlogin);
+		
+		loginbutton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				String entusername = usernameedittext.getText().toString();
+				String entpassword = passwordedittedt.getText().toString();
+				
+				if(entusername.equalsIgnoreCase(stUsername)&&entpassword.equals(stPassword))
+				{
+					Log.d("[DEBUG]", "Login Success");
+				}
+				
+				else
+				{
+					Log.d("[DEBUG]", "Login failed");
+				}
+			}
+		});
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+
 
 }
